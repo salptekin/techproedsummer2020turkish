f0c4b4af91d4ee7ce1b9cb26f46f5568d369d9e5
apple <apple@apples-imac-6.attlocal.net>
2020-05-08 22:34:51 -0400

    Turkce Java Summer 2020

---
+++ src/day05operators/Odev01.java
@@ -0,0 +1,16 @@
+package day05operators;
+
+public class Odev01 {
+
+	public static void main(String[] args) {
+		int numA = 2;
+		int numB = 3;
+		String str1 = "Study";
+		String str2 = "Hard";
+		//Ekran ciktisi 61 Study-1
+		
+		System.out.println((numB*numA) + "" + (numB-numA) + " " + str1 + (numA-numB));
+
+	}
+
+}
